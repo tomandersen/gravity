f97a5f82839a6353fc68306fab3cb43ad38be37d
Tom Andersen <tom@palmerandersen.com>
2016-10-12 23:53:38 -0400

    added new file, class for a box of 4d space

---
+++ src/box/Box4.cpp
@@ -0,0 +1,19 @@
+//
+//  Box4.cpp
+//  d4
+//
+//  Created by Tom Andersen on 2016-10-12.
+//  Copyright © 2016 Tom Andersen. All rights reserved.
+//
+
+#include "Box4.hpp"
+// constructor of Box4,
+Box4::Box4(long inSize)
+{
+    size = inSize;
+}
+
+Box4::~Box4()          
+{
+}
+
---
+++ src/box/Box4.hpp
@@ -0,0 +1,31 @@
+//
+//  Box4.hpp
+//  d4
+//
+//  Created by Tom Andersen on 2016-10-12.
+//  Copyright © 2016 Tom Andersen. All rights reserved.
+//
+
+#ifndef Box4_hpp
+#define Box4_hpp
+
+#include <stdio.h>
+
+// Box4 - 4D box of space.
+// inSize - 4, 8, 16, 32 or 64 or even 128 if you have a lot of memory
+// A box of size 8 has 8^4 points, each of which has six scalars. (the h)
+// This data is stored in xyz planes - six for each t, (since six components of the linearized metric in a minkowski space)
+// So for Box4 of size 8 we will need 8*6 = 48 zfp compressed arrays, (we work in uncompressed, store as compressed).
+
+
+class Box4
+{
+public:
+    Box4(long inSize);
+    ~Box4();
+    
+private:
+    long size;
+};
+
+#endif /* Box4_hpp */
